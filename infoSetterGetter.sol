b897e4c67025d66c1d70b9531c2f5bc8c59f6fb2
link375 <https://github.com/link375>
2018-07-04 09:49:32 -0600

    second test smart contract

---
+++ infoSetterGetter.sol
@@ -0,0 +1,37 @@
+// set the version of solidity you're using
+pragma solidity ^0.4.0;
+
+contract Info{
+	
+	// define and instantiate vars
+	string name = "0";
+	int age = 0;
+
+	// NAME FUNCTIONS
+	
+	// setter
+	// define the function and it's params
+	function setName(string _input){
+		// set the input param to name;
+		name = _input;
+	}
+
+	// getter
+	function getName() view returns (string){
+		return _name;
+	}
+
+	// AGE FUNCTIONS
+
+	// setter
+	function setAge(int _input){
+		// set the input param to name;
+		age = _input;
+	}
+
+	// getter
+	function getAge() view returns (int){
+		return _age;
+	}
+
+}
