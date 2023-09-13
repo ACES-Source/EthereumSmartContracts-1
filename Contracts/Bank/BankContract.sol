d7256ca7e3ffd46b650be024ac0b235d305dfc16
Devon Urbigkeit <55683173+ACES-Source@users.noreply.github.com>
2023-09-13 03:32:51 -0400

    Update BankContract.sol

--- Contracts/Bank/BankContract.sol
+++ Contracts/Bank/BankContract.sol
@@ -1,15 +1,21 @@
 pragma solidity ^0.4.0;
 
-// let users deposit eth and view their balance
+// let users deposit, withdraw, and transfer eth and view their balance
 
 contract BankContract{
 
 	uint balance;
+        address payable;
 
 	function deposit() payable {
 		balance += msg.value;
 	}
-
+        function withdraw() payable {
+		balance -= msg.value;
+	}
+        function transfer() payable {
+		balance -= msg.value;
+	}
 	function getBalance() view returns(uint){
 		return balance;
 	}
